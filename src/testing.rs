7659c55977a00216e9330888663f9dc36b3ff7a7
Mark Ross <markross470@gmail.com>
2023-10-06 16:21:47 +0100

    Fixed test errors when on non-default feature sets

--- src/testing.rs
+++ src/testing.rs
@@ -1,3 +1,15 @@
+#[cfg(not(feature = "builtin_lock"))]
+use crate::types::AcpiCpuFlags;
+#[cfg(not(all(
+    feature = "builtin_cache",
+    feature = "builtin_lock",
+    feature = "builtin_semaphore"
+)))]
+use core::ffi::c_void;
+
+#[cfg(not(feature = "builtin_alloc"))]
+use crate::types::AcpiAllocationError;
+
 use alloc::boxed::Box;
 
 use crate::{handler::AcpiHandler, status::AcpiError};
@@ -16,7 +28,8 @@ pub struct DummyHandler<'a> {
     pub fn_table_override: Box<
         dyn Fn(
             &crate::types::AcpiTableHeader,
-        ) -> Result<Option<crate::types::AcpiTableHeader<'a>>, crate::status::AcpiError>,
+        )
+            -> Result<Option<crate::types::AcpiTableHeader<'a>>, crate::status::AcpiError>,
     >,
 
     pub fn_physical_table_override: Box<
@@ -59,6 +72,51 @@ pub struct DummyHandler<'a> {
         Box<dyn Fn(crate::types::AcpiThreadCallback) -> Result<(), crate::status::AcpiError>>,
 
     pub fn_printf: Box<dyn Fn(core::fmt::Arguments)>,
+
+    #[cfg(not(feature = "builtin_cache"))]
+    pub fn_create_cache: Box<dyn Fn(&str, u16, u16) -> Result<*mut c_void, AcpiError>>,
+
+    #[cfg(not(feature = "builtin_cache"))]
+    pub fn_delete_cache: Box<dyn Fn(*mut c_void) -> Result<(), AcpiAllocationError>>,
+
+    #[cfg(not(feature = "builtin_cache"))]
+    pub fn_purge_cache: Box<dyn Fn(*mut c_void)>,
+
+    #[cfg(not(feature = "builtin_cache"))]
+    pub fn_acquire_object: Box<dyn Fn(*mut c_void) -> Option<*mut u8>>,
+
+    #[cfg(not(feature = "builtin_cache"))]
+    pub fn_release_object: Box<dyn Fn(*mut c_void, *mut u8)>,
+
+    #[cfg(not(feature = "builtin_lock"))]
+    pub fn_create_lock: Box<dyn Fn() -> Result<*mut c_void, AcpiError>>,
+
+    #[cfg(not(feature = "builtin_lock"))]
+    pub fn_delete_lock: Box<dyn Fn(*mut c_void)>,
+
+    #[cfg(not(feature = "builtin_lock"))]
+    pub fn_acquire_lock: Box<dyn Fn(*mut c_void) -> AcpiCpuFlags>,
+
+    #[cfg(not(feature = "builtin_lock"))]
+    pub fn_release_lock: Box<dyn Fn(*mut c_void, AcpiCpuFlags)>,
+
+    #[cfg(not(feature = "builtin_semaphore"))]
+    pub fn_create_semaphore: Box<dyn Fn(u32, u32) -> Result<*mut c_void, AcpiError>>,
+
+    #[cfg(not(feature = "builtin_semaphore"))]
+    pub fn_delete_semaphore: Box<dyn Fn(*mut c_void) -> Result<(), AcpiError>>,
+
+    #[cfg(not(feature = "builtin_semaphore"))]
+    pub fn_wait_semaphore: Box<dyn Fn(*mut c_void, u32, u16) -> Result<(), AcpiError>>,
+
+    #[cfg(not(feature = "builtin_semaphore"))]
+    pub fn_signal_semaphore: Box<dyn Fn(*mut c_void, u32) -> Result<(), AcpiError>>,
+
+    #[cfg(not(feature = "builtin_alloc"))]
+    pub fn_allocate: Box<dyn Fn(usize) -> Result<*mut u8, AcpiAllocationError>>,
+
+    #[cfg(not(feature = "builtin_alloc"))]
+    pub fn_free: Box<dyn Fn(*mut u8)>,
 }
 
 // SAFETY: This struct is only used in tests, which are carried out in a no_std environment not linked to any OS
@@ -72,20 +130,74 @@ impl<'a> DummyHandler<'a> {
             fn_map_memory: Box::new(|_, _| panic!("Dummy function on test struct called")),
             fn_unmap_memory: Box::new(|_, _| panic!("Dummy function on test struct called")),
             fn_get_physical_address: Box::new(|_| panic!("Dummy function on test struct called")),
-            fn_install_interrupt_handler: Box::new(|_, _| panic!("Dummy function on test struct called")),
-            fn_remove_interrupt_handler: Box::new(|_, _| panic!("Dummy function on test struct called")),
+            fn_install_interrupt_handler: Box::new(|_, _| {
+                panic!("Dummy function on test struct called")
+            }),
+            fn_remove_interrupt_handler: Box::new(|_, _| {
+                panic!("Dummy function on test struct called")
+            }),
             fn_get_thread_id: Box::new(|| panic!("Dummy function on test struct called")),
             fn_execute: Box::new(|_| panic!("Dummy function on test struct called")),
             fn_printf: Box::new(|_| panic!("Dummy function on test struct called")),
-            fn_initialize: Box::new(||panic!("Dummy function on test struct called")),
-            fn_terminate: Box::new(||panic!("Dummy function on test struct called")),
-            fn_predefined_override: Box::new(|_|panic!("Dummy function on test struct called")),
-            fn_table_override: Box::new(|_|panic!("Dummy function on test struct called")),
-            fn_physical_table_override: Box::new(|_|panic!("Dummy function on test struct called")),
+            fn_initialize: Box::new(|| panic!("Dummy function on test struct called")),
+            fn_terminate: Box::new(|| panic!("Dummy function on test struct called")),
+            fn_predefined_override: Box::new(|_| panic!("Dummy function on test struct called")),
+            fn_table_override: Box::new(|_| panic!("Dummy function on test struct called")),
+            fn_physical_table_override: Box::new(|_| {
+                panic!("Dummy function on test struct called")
+            }),
+
+            #[cfg(not(feature = "builtin_cache"))]
+            fn_create_cache: Box::new(|_, _, _| panic!("Dummy function on test struct called")),
+        
+            #[cfg(not(feature = "builtin_cache"))]
+            fn_delete_cache: Box::new(|_| panic!("Dummy function on test struct called")),
+        
+            #[cfg(not(feature = "builtin_cache"))]
+            fn_purge_cache: Box::new(|_| panic!("Dummy function on test struct called")),
+        
+            #[cfg(not(feature = "builtin_cache"))]
+            fn_acquire_object: Box::new(|_| panic!("Dummy function on test struct called")),
+        
+            #[cfg(not(feature = "builtin_cache"))]
+            fn_release_object: Box::new(|_, _| panic!("Dummy function on test struct called")),
+        
+            #[cfg(not(feature = "builtin_lock"))]
+            fn_create_lock: Box::new(|| panic!("Dummy function on test struct called")),
+        
+            #[cfg(not(feature = "builtin_lock"))]
+            fn_delete_lock: Box::new(|_| panic!("Dummy function on test struct called")),
+        
+            #[cfg(not(feature = "builtin_lock"))]
+            fn_acquire_lock: Box::new(|_| panic!("Dummy function on test struct called")),
+        
+            #[cfg(not(feature = "builtin_lock"))]
+            fn_release_lock: Box::new(|_, _| panic!("Dummy function on test struct called")),
+        
+            #[cfg(not(feature = "builtin_semaphore"))]
+            fn_create_semaphore: Box::new(|_, _| panic!("Dummy function on test struct called")),
+        
+            #[cfg(not(feature = "builtin_semaphore"))]
+            fn_delete_semaphore: Box::new(|_| panic!("Dummy function on test struct called")),
+        
+            #[cfg(not(feature = "builtin_semaphore"))]
+            fn_wait_semaphore: Box::new(|_, _, _| panic!("Dummy function on test struct called")),
+        
+            #[cfg(not(feature = "builtin_semaphore"))]
+            fn_signal_semaphore: Box::new(|_, _| panic!("Dummy function on test struct called")),
+        
+            #[cfg(not(feature = "builtin_alloc"))]
+            fn_allocate: Box::new(|_| panic!("Dummy function on test struct called")),
+        
+            #[cfg(not(feature = "builtin_alloc"))]
+            fn_free: Box::new(|_| panic!("Dummy function on test struct called")),
+
         }
     }
 }
 
+// SAFETY: 
+// Each method in this implementation is the user of the test struct's responsibility
 unsafe impl<'a> AcpiHandler for DummyHandler<'a> {
     fn get_root_pointer(&mut self) -> crate::types::AcpiPhysicalAddress {
         (self.fn_get_root_pointer)()
@@ -169,5 +281,98 @@ unsafe impl<'a> AcpiHandler for DummyHandler<'a> {
         table: &crate::types::AcpiTableHeader,
     ) -> Result<Option<(crate::types::AcpiPhysicalAddress, u32)>, crate::status::AcpiError> {
         (self.fn_physical_table_override)(table)
+    }
+
+    #[cfg(not(feature = "builtin_cache"))]
+    unsafe fn create_cache(
+        &mut self,
+        cache_name: &str,
+        object_size: u16,
+        max_depth: u16,
+    ) -> Result<*mut c_void, AcpiError> {
+        (self.fn_create_cache)(cache_name, object_size, max_depth)
+    }
+
+    #[cfg(not(feature = "builtin_cache"))]
+    unsafe fn delete_cache(&mut self, cache: *mut c_void) -> Result<(), AcpiAllocationError> {
+        (self.fn_delete_cache)(cache)
+    }
+
+    #[cfg(not(feature = "builtin_cache"))]
+    unsafe fn purge_cache(&mut self, cache: *mut c_void) {
+        (self.fn_purge_cache)(cache)
+    }
+
+    #[cfg(not(feature = "builtin_cache"))]
+    unsafe fn acquire_object(&mut self, cache: *mut c_void) -> Option<*mut u8> {
+        (self.fn_acquire_object)(cache)
+    }
+
+    #[cfg(not(feature = "builtin_cache"))]
+    unsafe fn release_object(&mut self, cache: *mut c_void, object: *mut u8) {
+        (self.fn_release_object)(cache, object)
+    }
+
+    #[cfg(not(feature = "builtin_lock"))]
+    unsafe fn create_lock(&mut self) -> Result<*mut c_void, AcpiError> {
+        (self.fn_create_lock)()
+    }
+
+    #[cfg(not(feature = "builtin_lock"))]
+    unsafe fn delete_lock(&mut self, lock: *mut c_void) {
+        (self.fn_delete_lock)(lock)
+    }
+
+    #[cfg(not(feature = "builtin_lock"))]
+    unsafe fn acquire_lock(&mut self, handle: *mut c_void) -> AcpiCpuFlags {
+        (self.fn_acquire_lock)(handle)
+    }
+
+    #[cfg(not(feature = "builtin_lock"))]
+    unsafe fn release_lock(&mut self, handle: *mut c_void, flags: AcpiCpuFlags) {
+        (self.fn_release_lock)(handle, flags)
+    }
+
+    #[cfg(not(feature = "builtin_semaphore"))]
+    unsafe fn create_semaphore(
+        &mut self,
+        max_units: u32,
+        initial_units: u32,
+    ) -> Result<*mut c_void, AcpiError> {
+        (self.fn_create_semaphore)(max_units, initial_units)
+    }
+
+    #[cfg(not(feature = "builtin_semaphore"))]
+    unsafe fn delete_semaphore(&mut self, handle: *mut c_void) -> Result<(), AcpiError> {
+        (self.fn_delete_semaphore)(handle)
+    }
+
+    #[cfg(not(feature = "builtin_semaphore"))]
+    unsafe fn wait_semaphore(
+        &mut self,
+        handle: *mut c_void,
+        units: u32,
+        timeout: u16,
+    ) -> Result<(), AcpiError> {
+        (self.fn_wait_semaphore)(handle, units, timeout)
+    }
+
+    #[cfg(not(feature = "builtin_semaphore"))]
+    unsafe fn signal_semaphore(
+        &mut self,
+        handle: *mut c_void,
+        units: u32,
+    ) -> Result<(), AcpiError> {
+        (self.fn_signal_semaphore)(handle, units)
+    }
+
+    #[cfg(not(feature = "builtin_alloc"))]
+    unsafe fn allocate(&mut self, size: usize) -> Result<*mut u8, AcpiAllocationError> {
+        (self.fn_allocate)(size)
+    }
+
+    #[cfg(not(feature = "builtin_alloc"))]
+    unsafe fn free(&mut self, memory: *mut u8) {
+        (self.fn_free)(memory)
     }
 }
